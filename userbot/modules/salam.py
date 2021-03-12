8ca10b20181a8e9a710d285c934794debe7e7c93
xrrmli <80378609+xrrmli@users.noreply.github.com>
2021-03-12 21:19:22 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -16,25 +16,25 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamualaikum Cantikk🥰")
+    await typew.edit("Assalamualaikum kawann")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumussalam Ya ahli kubur")
+    await typew.edit("Wa'alaikumussalam bangsatt")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumussalam Cantikkk🥰🥰.")
+    await typew.edit("Wa'alaikumussalam kawann")
 
 
 CMD_HELP.update({
     "salam":
     "`.P`\
-\nUsage: Untuk Memberi salam.\
+\nUsage: p ga toxic P toxic.\
 \n\n`.L`\
-\nUsage: Untuk Menjawab Salam."
+\nUsage: l ga toxic L toxic."
 })
