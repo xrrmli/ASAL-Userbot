98b93527a84a4895d3ff44fa2bdbd7cdc9d0ce6f
xrrmli <80378609+xrrmli@users.noreply.github.com>
2021-03-15 01:01:25 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "༒︎ **Repo Userbot:** [Crash-Userbot](https://github.com/xrrmli/Crash-Userbot)\n༒︎ **Pemilik:** [Ramli](t.me/mentalbrikden)"
+        "❖ **Repo Userbot:** [Crash-Userbot](https://github.com/xrrmli/Crash-Userbot)\n❖ **Pemilik:** [Ramli](t.me/mentalbrikden)"
     )
 
 
