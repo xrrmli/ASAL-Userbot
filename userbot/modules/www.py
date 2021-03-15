29e03ff440671989982b4ed1cadbcdbd092a1ef7
xrrmli <80378609+xrrmli@users.noreply.github.com>
2021-03-16 02:38:18 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -45,7 +45,7 @@ async def get_readable_time(seconds: int) -> str:
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     await get_readable_time((time.time() - StartTime))
-    start = datetime.now()await pong.edit("`LOADING...`")
+    start = datetime.now()
     await pong.edit("**100% ██████████**")
     await pong.edit("**80% ████████▒▒**")
     await pong.edit("**60% ██████▒▒▒▒**")
