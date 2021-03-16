0c614602c81b83e96ea68b14412a066b172882a1
xrrmli <80378609+xrrmli@users.noreply.github.com>
2021-03-16 14:50:46 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -67,7 +67,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         await event.edit('`[HEROKU]:'
-                         '\nNunggu Dia Yg Pura Pura Ga Peka Aja Sabar, Masa Nunggu Ginian Gasabaran Kontol`'
+                         '\nNunggu Dia Yg Ga Peka Aja Sabar, Masa Nunggu Ginian Gasabaran Kontol`'
                          )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
