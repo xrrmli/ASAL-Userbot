f3fe5feb873ac24e5407adf375aa8f738c54500a
xrrmli <80378609+xrrmli@users.noreply.github.com>
2021-03-18 13:41:24 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -27,9 +27,9 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t☠️  "
+            string += "`\t⌬  "
         await event.edit("**❉ Daftar Perintah Untuk\nCrash-Userbot:\n\n**"
-                         f"☠️{string}☠️"
+                         f"⌬{string}⌬"
                          "\n▰▱▰▱▰▱▰▱▰▱▰▰▱▰▱▰▱▰▱▰")
         await event.reply(f"\n**Ketik Contoh** `.help afk` **Untuk Informasi Perintah**")
         await asyncio.sleep(1000)
